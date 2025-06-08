c982e458114b1a9ff657891627802bee482db25e
longindev <fhazlt303@gmail.com>
2025-06-09 08:22:18 +0900

    P120813. 짝수는 싫어요 - refactoring - IntStream.rangeClosed(int startNum, int endNum) : startNum, endNum 포함해서 startNum부터 endNum까지 숫자 Stream 반환

--- algorithm/java/src/intro/P120813.java
+++ algorithm/java/src/intro/P120813.java
@@ -1,27 +1,14 @@
 package intro;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class P120813 {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 15;
         System.out.println("result = "+ Arrays.toString(solution(n)));
     }
     static int[] solution(int n) {
-        int index = n/2;
-
-        if(n % 2 != 0){
-            return resultArray(n, index);
-        }
-        return resultArray(n-1, index-1);
-    }
-    static int[] resultArray(int n, int index){
-        int[] arr = new int[index+1];
-        while(n >0){
-            arr[index] = n;
-            n-=2;
-            index-=1;
-        }
-        return arr;
+        return IntStream.rangeClosed(0, n).filter(i -> i % 2 == 1).toArray();
     }
 }
