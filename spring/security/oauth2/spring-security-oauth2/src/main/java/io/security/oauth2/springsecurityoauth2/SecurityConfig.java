9168dd338f10c15dcf15a2364308bdacc6f79d53
LogInDev <fhazlt303@gmail.com>
2025-06-22 08:10:44 +0900

    HttpBasic 인증 처리 - session에 담을 때 처리와 담지 않을 때 처리가 다르다 - *EntryPoint를 커스텀한다고 해도 규약은 지켜줘야 동작된다. (ex: response.addHeader에 키값이 달라지면 화면이 안뜸)

--- spring/security/oauth2/spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ spring/security/oauth2/spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,14 +21,8 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
-        http.formLogin();
-        http.httpBasic();
-        http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
-            @Override
-            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-                System.out.println("custom entryPoint");
-            }
-        });
+        http.httpBasic().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 
