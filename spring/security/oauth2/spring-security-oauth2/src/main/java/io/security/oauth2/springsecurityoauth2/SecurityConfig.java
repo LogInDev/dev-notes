511a80a82f4cc7eee847aa2bf79ca4939fb146a0
LogInDev <fhazlt303@gmail.com>
2025-06-21 16:56:09 +0900

    CustomSecurityConfigurer 생성 - AbstractHttpConfigurer을 상속받은 CustomSecurityConfigurer를 생성해서 init 메소드와 configure 메소드를 커스텀해서 SecurityConfig에 적용

---
+++ spring/security/oauth2/spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -0,0 +1,21 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+//@EnableWebSecurity    // 현재 Bean을 만들어서 설정하기 때문에 둘 다 가능
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+        http.formLogin();
+        http.apply(new CustomSecurityConfigurer().setFlag(false));
+        return http.build();
+    }
+
+}
