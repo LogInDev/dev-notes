e49314c042ca5e45903df55d544ffac7e5291f6d
SaeRom Choi <fhazlt303@gmail.com>
2025-10-01 11:54:30 +0900

    slimpopup.js 업데이트

--- react/flexible-sim/temp/slimpopup.js
+++ react/flexible-sim/temp/slimpopup.js
@@ -1,35 +1,63 @@
-import React, { Component, createRef } from 'react';
-const slimscroll = require('util/slimscroll-fixed'); // ← 위 파일 경로
+// AssistantPopup.render() 내부
+                <AIPopupMessageList
+                  channelinfo={this.props.infosummary}
+                  channelid={popupId}
+                  isPopup={true}
+                  messages={threadMsg}
+                  userid={this.props.profile.userID}
+                  companycode={this.props.profile.companyCode}
+                  action={this.props.action}
+                  onAddMention={this.setMentionToInputBox.bind(this)}
+                  openDetail={this.openDetail}
+                  openPopupFlag={false}
+                  openMessageTerm={this.openMessageTerm.bind(this)}
++                 popupDocument={this.props.popupWindow && this.props.popupWindow.document}
++                 popupId={popupId}
+                  fontcolor={
+                    this.props.infosummary.channel_set !== undefined &&
+                    this.props.infosummary.channel_set.CHANNELID !== -1
+                      ? this.props.infosummary.channel_set.COLOR_FONT
+                      : global.CONFIG.other.fontColor
+                  }
+                />
+                
+                
 
-export default class PopupContent extends Component {
-  constructor(props) {
-    super(props);
-    this.ref = createRef();
-  }
-  componentDidMount() {
-    this.raf = requestAnimationFrame(() => {
-      const el = this.ref.current;
-      if (!el) return;
 
-      // 팝업 문서 기준으로 elementsArray 전달 (ownerDocument 자동 인식)
-      slimscroll({ idSelector: '', height: '100%', wheelStep: 30 }, [el]);
+- <div id="message3" key="message3" className="chatlistin" ref="chatlistin3">
++ <div
++   id={`aiviewMsg-${this.props.popupId}`}
++   data-mid="message3"             // (옵션) 필요하면 유지용
++   className="chatlistin"
++   ref="chatlistin3"
++ >
 
-      // 디버그: 팝업 컨텍스트면 true/true
-      // console.log('doc/window in popup?', el.ownerDocument !== document, (el.ownerDocument.defaultView !== window));
-    });
-  }
-  componentWillUnmount() {
-    cancelAnimationFrame(this.raf);
-    // destroy는 현재 코드에 옵션으로 처리(기존 패턴 유지): 다시 init 시 options={ destroy:true }로 호출하도록 필요시 확장
-  }
-  render() {
-    return (
-      <div style={{ height: '100%' }}>
-        <div ref={this.ref} style={{ height: '100%' }}>
-          {/* 긴 메시지들 */}
-          {this.props.children}
-        </div>
-      </div>
-    );
-  }
+- import slimscroll from 'util/slimscroll';
++ import slimscroll from 'util/slimscroll-fixed';
+
+// ...
+moveScroll(height) {
+-  this.slimscroll = new slimscroll({
+-    height: '100%',
+-    idSelector: '#message3',
+-    scrollTo: height || '100000'
+-  });
+-  this.slimscroll.init();
++  const el = this.refs.chatlistin3;
++  if (!el) return;
++  // 팝업 문서 우선, 없으면 ownerDocument
++  const doc = (this.props.popupDocument) || (el.ownerDocument);
++  // 최초 래핑(이미 래핑되어 있으면 생략됨)
++  if (!this._inited) {
++    const init = new slimscroll({ height: '100%', doc }, [el]); // 요소 배열 + doc
++    init.init();
++    this._inited = true;
++  }
++  // 스크롤 이동
++  const scroller = new slimscroll({ scrollTo: height || '100000', doc }, [el]);
++  scroller.init();
+
+  this.prevScrollHeight = this.refs.chatlistin3.scrollHeight;
+  this.precurrentScrollTop = this.refs.chatlistin3.scrollTop;
+  // ...
 }
