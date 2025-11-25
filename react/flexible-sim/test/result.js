7fae7025f544a1eb7adc42a0ccf6da22c82fa9eb
SaeRom Choi <fhazlt303@gmail.com>
2025-11-25 10:34:35 +0900

    Update result.js

--- react/flexible-sim/test/result.js
+++ react/flexible-sim/test/result.js
@@ -1,69 +1,53 @@
-AIUThreadItem.js에서 x버튼을 누르면 모달창이 열리고 확인을 클릭하면 explosion이 실행되면서 deleteAITHread함수가 실행되는 아래처럼 THREAD2031라는 tp코드가 와서 처리하는거야.
-  근데 일단 에러가 나느데 왜ㄴ나느거야?
+열린거 또 s열리게 MAp으로 관리하고 있는데 뭔가 열어도 map에 적용이 안되는 경우가 있는거 같아. 정확하게 동작되게 수정해
 
-delete하기=------ 700002921
-aiThread.js:451 delete하기=------ (5) [{…}, {…}, {…}, {…}, {…}]
-aiThread.js:452 delete하기=------ []
-index.js:45 {uiSetting: {…}, profile: {…}, channel: {…}, messages: {…}, search: {…}, …}
-installHook.js:1 Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in the componentWillUnmount method.
-    in AIThreadItem (at AIThreadList.js:125)
+class ApiHandler {
+  constructor(sock) {
+    this.sock = sock;
+    this.store = Store.getStore();
 
-  
-case 'THREAD2031': // AI THREAD 삭제
-          console.log('000000000', msg)
-            const threadId = msg.channel_id + ''
-            // store.dispatch(actions.deleteAIMessage(threadId));
-            // store.dispatch(actions.deleteAIThread(threadId));
-
-              try {
-    store.dispatch(actions.deleteAIMessage(threadId));
-    console.log('durl-pre-deleteAIThread---'); // 이 로그가 찍히나요?
-  } catch (error) {
-    console.error('Dispatch 중 에러 발생:', error); // 에러가 발생하면 이 로그가 찍힐 것입니다.
+    this.userid = this.store.getState().profile.profile.userID;
+    this.airunner = new airunner();
+    this.threadRegisterMap = new Map();
   }
+  openAIThread(threadId, callback) {
+    let _this = this;
+    const isOpened = this.isThreadRegistered(threadId);
+    
+    if(!isOpened){
+      let addr = 'hynix.client.' + threadId;
 
-              try {
-    store.dispatch(actions.deleteAIThread(threadId));
-    console.log('durl-post-deleteAIThread---'); // 이 로그가 찍히나요?
-  } catch (error) {
-    console.error('Dispatch 중 에러 발생:', error); // 에러가 발생하면 이 로그가 찍힐 것입니다.
-  }
-
-            console.log('durl-0------여기안와-----')
-            let { dmchannels, currentChannel } = store.getState().aiThread;
-            let { detail_selected } = store.getState().uiSetting;
-            
-            console.log('durl-0-----------', currentChannel)
-            console.log('durl-0-----------', threadId)
-            console.log('durl-0-----------', currentChannel + '' === threadId + '')
-            if(currentChannel + '' === threadId + '' ){
-              console.log('durl-0-----------')
-              if(dmchannels.length > 0 ){
-                console.log('durl-1-----------')
-                let openThreadId = currentChannel === dmchannels[0].channel_id ? dmchannels[1].channel_id : dmchannels[0].channel_id
-                this.openAIThreadPanel(dmchannels[0].channel_id);
-              }else{
-                if(detail_selected === 'assistant') store.dispatch(actions.clickDetailTabItem('info'));
-              }
-            }
-            // this.requestAiThreadlDelete(threadId);
-            break;
-
-
-action에서 
-
-  case ActionTypes.DELETE_AITHREAD:{
-    let deleteThreadId = action.thread_id + '';
-    let prevThreadList = state.dmchannels.filter(thread =>thread.channel_id !== deleteThreadId);
-    let prevCurrentThreads = state.currentThreads.filter(threadId =>threadId !== deleteThreadId);
-
-    console.log('delete하기=------', deleteThreadId)
-    console.log('delete하기=------', prevThreadList)
-    console.log('delete하기=------', prevCurrentThreads)
-
-    return {
-      ...state,
-      dmchannels:prevThreadList,
-      currentThreads:prevCurrentThreads
+      let header = {
+        client: 0,
+        UUID: global.Common.getCookie('sessionkey'),
+      };
+      
+      const handler = (msg) => {
+        if (typeof callback === 'function') callback(threadId, msg);
+      }
+      
+      _this.threadRegisterMap.set(threadId, true);
+      _this.sock.register(addr, header, handler);
     }
   }
+  
+  closeAIThread(threadid, callback) {
+    let _this = this;
+    const isOpened = this.isThreadRegistered(threadid);
+    
+    if(isOpened){
+          
+      let header = {
+        client: 0,
+        UUID: global.Common.getCookie('sessionkey'),
+      };
+
+      let addr = 'hynix.client.' + threadid;
+
+      this.threadRegisterMap.delete(threadid);
+      _this.sock.unregister(addr, header, (msg) => {
+        if (typeof callback === 'function') callback(msg);
+      });
+    }
+  }
+
+}
