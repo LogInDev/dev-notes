54e410e66765a9c5814e905b44e7a38f8e0d674c
LogInDev <fhazlt303@gmail.com>
2026-01-12 11:52:54 +0900

    timer challenge UI 추가

--- react/udemy-course/ref-portal-demo/src/App.jsx
+++ react/udemy-course/ref-portal-demo/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChanllenger from './components/TimerChallenger.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChanllenger title="Easy" targetTime={1} />
+        <TimerChanllenger title="Not easy" targetTime={5} />
+        <TimerChanllenger title="Getting tough" targetTime={10} />
+        <TimerChanllenger title="Pros Only" targetTime={15} />
+      </div>
     </>
   );
 }
