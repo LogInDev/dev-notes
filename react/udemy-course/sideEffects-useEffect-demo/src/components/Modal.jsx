7ea1911d09f8df6ec723f26d33d054f8a846cfaf
LogInDev <fhazlt303@gmail.com>
2026-01-13 14:11:28 +0900

    fix: useEffect 의존성 배열에 open 추가하여 modal 동작 정상화

--- react/udemy-course/sideEffects-useEffect-demo/src/components/Modal.jsx
+++ react/udemy-course/sideEffects-useEffect-demo/src/components/Modal.jsx
@@ -10,10 +10,10 @@ export default function Modal({ open, children }) {
     } else {
       dialog.current.close();
     }
-  }, []);
+  }, [open]);
 
   return createPortal(
-    <dialog className="modal" ref={dialog} open={open}>
+    <dialog className="modal" ref={dialog}>
       {children}
     </dialog>,
     document.getElementById('modal')
