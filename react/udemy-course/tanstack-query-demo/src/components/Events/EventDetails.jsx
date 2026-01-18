3ef21f7b725299d26dcd09f3886921b2b9bd8631
LogInDev <fhazlt303@gmail.com>
2026-01-18 13:40:54 +0900

    feat: 이벤트 삭제 전 확인 모달 추가 및 삭제 UX 개선

--- react/udemy-course/tanstack-query-demo/src/components/Events/EventDetails.jsx
+++ react/udemy-course/tanstack-query-demo/src/components/Events/EventDetails.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@tanstack/react-query';
 
 import Header from '../Header.jsx';
 import { fetchEvent, deleteEvent, queryClient } from '../../util/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
+import Modal from '../UI/Modal.jsx';
 
 export default function EventDetails() {
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const params = useParams();
   const navigate = useNavigate();
 
@@ -15,16 +19,32 @@ export default function EventDetails() {
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id })
   });
 
-  const { mutate } = useMutation({
-    mutationFn: deleteEvent,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ['events'],
-        refetchType: 'none'
-      });
-      navigate('/events');
-    }
-  })
+  const {
+    mutate,
+    isPending: isPendingDeletion,
+    isError: isErrorDeleting,
+    error: deleteError } = useMutation({
+      mutationFn: deleteEvent,
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ['events'],
+          refetchType: 'none'
+        });
+        navigate('/events');
+      }
+    });
+
+  function handleStartDelete() {
+    setIsDeleting(true);
+  }
+
+  function handleStopDelete() {
+    setIsDeleting(false);
+  }
+
+  function handleDelete() {
+    mutate({ id: params.id });
+  }
 
   let content;
 
@@ -59,7 +79,7 @@ export default function EventDetails() {
         <header>
           <h1>{data.title}</h1>
           <nav>
-            <button onClick={handleDelete}>Delete</button>
+            <button onClick={handleStartDelete}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
@@ -77,12 +97,30 @@ export default function EventDetails() {
     );
   }
 
-  function handleDelete() {
-    mutate({ id: params.id });
-  }
-
   return (
     <>
+      {isDeleting && (
+        <Modal onClose={handleStopDelete}>
+          <h2>Are you sure?</h2>
+          <p>Do you really want to delete this event? This action caanot be undone.</p>
+          <div className="form-actions">
+            {isPendingDeletion && <p>isDeleting, please wait...</p>}
+            {!isPendingDeletion && <>
+              <button onClick={handleStopDelete} className="button-text">
+                Cancel
+              </button>
+              <button onClick={handleDelete} className="button">
+                Delete
+              </button>
+            </>}
+          </div>
+          {isErrorDeleting &&
+            <ErrorBlock
+              title="Failed to delete event"
+              message={deleteError.info?.message || 'Failed to delete event, please try again later.'} />
+          }
+        </Modal>
+      )}
       <Outlet />
       <Header>
         <Link to="/events" className="nav-item">
