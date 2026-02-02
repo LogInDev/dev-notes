72ca8a04d3949fa2ac5495aeaa29c6cf9a7025e8
LogInDev <fhazlt303@gmail.com>
2026-02-03 04:26:14 +0900

    docs: 도메일 모델 다이어그램 및 문서 추가

--- algorithm/java/src/intro/I1_1.java
+++ algorithm/java/src/intro/I1_1.java
@@ -17,11 +17,27 @@ public class I1_1 {
         int answer = 0;
 
         str = str.toLowerCase();
-        Map<Character, Integer> map = new HashMap<>();
+        // RESULT CASE 1
+//        Map<Character, Integer> map = new HashMap<>();
+//        for (int i = 0; i < str.length(); i++) {
+//            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
+//        }
+//        answer = map.getOrDefault(findStr.toLowerCase().charAt(0), 0);
+
+        // RESULT CASE 2
+//        for (char c : str.toCharArray()) {
+//            if (c == findStr.toLowerCase().charAt(0)) {
+//                answer++;
+//            }
+//        }
+
+        // 강의 풀이
+        str = str.toUpperCase();
+        char t = Character.toUpperCase(findStr.charAt(0));
         for (int i = 0; i < str.length(); i++) {
-            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
+             if(str.charAt(i) == t) answer++;
+
         }
-        answer = map.getOrDefault(findStr.toLowerCase().charAt(0), 0);
         return answer;
     }
 }
