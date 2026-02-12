455129008874ef6d04f2804570ad180cc93dadd3
SaeRom Choi <fhazlt303@gmail.com>
2026-02-12 12:26:53 +0900

    Update es.js

--- react/es.js
+++ react/es.js
@@ -1,130 +1,188 @@
-스프링은 이런코드로되어있어. 여기서 수정해
+아니 쿼리결과는  아래처럼나와 gpu 없는 경우. gpu있는 경우는 gpucount도 다나오고
 
-@Override
-    public List<ResourcesResponse> getResourcesList(Layout layout, ResourcesRequest request)  {
+{
+  "took" : 10,
+  "timed_out" : false,
+  "_shards" : {
+    "total" : 3,
+    "successful" : 3,
+    "skipped" : 0,
+    "failed" : 0
+  },
+  "hits" : {
+    "total" : {
+      "value" : 10000,
+      "relation" : "gte"
+    },
+    "max_score" : null,
+    "hits" : [
+      {
+        "_index" : "dashboard-resource-monthly-2026_new_temp",
+        "_type" : "_doc",
+        "_id" : "K1C4S5wBLBgxzfZ9RIi4",
+        "_score" : null,
+        "_source" : {
+          "cpu_allocated_usage_cores" : 0.0,
+          "memory_allocated_usage_percent" : 0.0,
+          "cpu_request_cores" : 0.0,
+          "label" : {
+            "rscId" : "123",
+            "namespace" : "123",
+            "svcType" : "app",
+            "prjId" : "123",
+            "env" : "dev",
+            "locId" : "null",
+            "target" : "1"
+          },
+          "memory_limit_bytes" : 0,
+          "cpu_count" : {
+            "usage_20_to_25" : 0,
+            "usage_25_to_30" : 0,
+            "usage_10_to_15" : 0,
+            "usage_35_to_40" : 0,
+            "usage_40_to_45" : 0,
+            "usage_15_to_20" : 0,
+            "usage_30_to_35" : 0,
+            "usage_5_to_10" : 0,
+            "usage_70_to_75" : 0,
+            "usage_75_to_80" : 0,
+            "usage_65_to_70" : 0,
+            "usage_80_to_85" : 0,
+            "usage_60_to_65" : 0,
+            "usage_0_to_5" : 0,
+            "usage_50_to_55" : 0,
+            "usage_45_to_50" : 0,
+            "usage_90_to_95" : 0,
+            "usage_95_to_100" : 0,
+            "usage_55_to_60" : 0,
+            "usage_85_to_90" : 0
+          },
+          "@timestamp" : "2026-02-11T17:01:30.583+0900",
+          "cpu_allocated_usage_percent" : 0.0,
+          "time_range" : {
+            "start" : "2026-02-01T00:00:00.000+0900",
+            "end" : "2026-03-01T00:00:00.000+0900"
+          },
+          "logdate" : "2026-02-01T00:00:00.000+0900",
+          "cpu_limit_cores" : 0.0,
+          "memory_allocated_usage_bytes" : 0,
+          "memory_count" : {
+            "usage_20_to_25" : 0,
+            "usage_25_to_30" : 0,
+            "usage_10_to_15" : 0,
+            "usage_35_to_40" : 0,
+            "usage_40_to_45" : 0,
+            "usage_15_to_20" : 0,
+            "usage_30_to_35" : 0,
+            "usage_5_to_10" : 0,
+            "usage_70_to_75" : 0,
+            "usage_75_to_80" : 0,
+            "usage_65_to_70" : 0,
+            "usage_80_to_85" : 0,
+            "usage_60_to_65" : 0,
+            "usage_0_to_5" : 0,
+            "usage_50_to_55" : 0,
+            "usage_45_to_50" : 0,
+            "usage_90_to_95" : 0,
+            "usage_95_to_100" : 0,
+            "usage_55_to_60" : 0,
+            "usage_85_to_90" : 0
+          },
+          "memory_request_bytes" : 0,
+          "db" : {
+            "cpu_limit_cores" : 0.10000000149011612,
+            "gpu_limit_devices" : 0.0,
+            "memory_limit_bytes" : 134217728
+          }
+        },
+        "sort" : [
+          "app"
+        ]
+      },
+      {
+        "_index" : "dashboard-resource-monthly-2026_new_temp",
+        "_type" : "_doc",
+        "_id" : "LFC4S5wBLBgxzfZ9RIi4",
+        "_score" : null,
+        "_source" : {
+          "cpu_allocated_usage_cores" : 0.0,
+          "memory_allocated_usage_percent" : 0.0,
+          "cpu_request_cores" : 0.0,
+          "label" : {
+            "rscId" : "123",
+            "namespace" : "123",
+            "svcType" : "app",
+            "prjId" : "123",
+            "env" : "prd",
+            "locId" : "null",
+            "target" : "1"
+          },
+          "memory_limit_bytes" : 0,
+          "cpu_count" : {
+            "usage_20_to_25" : 0,
+            "usage_25_to_30" : 0,
+            "usage_10_to_15" : 0,
+            "usage_35_to_40" : 0,
+            "usage_40_to_45" : 0,
+            "usage_15_to_20" : 0,
+            "usage_30_to_35" : 0,
+            "usage_5_to_10" : 0,
+            "usage_70_to_75" : 0,
+            "usage_75_to_80" : 0,
+            "usage_65_to_70" : 0,
+            "usage_80_to_85" : 0,
+            "usage_60_to_65" : 0,
+            "usage_0_to_5" : 0,
+            "usage_50_to_55" : 0,
+            "usage_45_to_50" : 0,
+            "usage_90_to_95" : 0,
+            "usage_95_to_100" : 0,
+            "usage_55_to_60" : 0,
+            "usage_85_to_90" : 0
+          },
+          "@timestamp" : "2026-02-11T17:01:30.584+0900",
+          "cpu_allocated_usage_percent" : 0.0,
+          "time_range" : {
+            "start" : "2026-02-01T00:00:00.000+0900",
+            "end" : "2026-03-01T00:00:00.000+0900"
+          },
+          "logdate" : "2026-02-01T00:00:00.000+0900",
+          "cpu_limit_cores" : 0.0,
+          "memory_allocated_usage_bytes" : 0,
+          "memory_count" : {
+            "usage_20_to_25" : 0,
+            "usage_25_to_30" : 0,
+            "usage_10_to_15" : 0,
+            "usage_35_to_40" : 0,
+            "usage_40_to_45" : 0,
+            "usage_15_to_20" : 0,
+            "usage_30_to_35" : 0,
+            "usage_5_to_10" : 0,
+            "usage_70_to_75" : 0,
+            "usage_75_to_80" : 0,
+            "usage_65_to_70" : 0,
+            "usage_80_to_85" : 0,
+            "usage_60_to_65" : 0,
+            "usage_0_to_5" : 0,
+            "usage_50_to_55" : 0,
+            "usage_45_to_50" : 0,
+            "usage_90_to_95" : 0,
+            "usage_95_to_100" : 0,
+            "usage_55_to_60" : 0,
+            "usage_85_to_90" : 0
+          },
+          "memory_request_bytes" : 0,
+          "db" : {
+            "cpu_limit_cores" : 0.10000000149011612,
+            "gpu_limit_devices" : 0.0,
+            "memory_limit_bytes" : 134217728
+          }
+        },
+        "sort" : [
+          "app"
+        ]
+      }
+    ]
+  }
+}
 
-        // 1. result 객체 생성
-        List<ResourcesResponse> result = new ArrayList<>();
-
-        // 2. searchParam check
-        List<String> searchParam = this.validSearchParam(request.getSearchParam());
-
-        // 3. 메트릭 조회 및 셋팅을 위한 객체 생성
-        List<String> prjIdList = this.getPrjIdList(layout, searchParam);
-        List<Map<String,String>> prjObjList = this.getPrjObjList(prjIdList);
-        List<ResourcesWithRatioEntity> metricList;
-        LocalDate date = request.getSearchMonth().atDay(1);
-
-        // 4. metric 조회 및 result data 셋팅
-        if (!prjIdList.isEmpty()) {
-            metricList = elasticsearchRepository.findResourcesByPrjId(
-                    prjIdList,
-                    date
-            );
-
-            for (ResourcesWithRatioEntity metric : metricList) {
-                if (metric != null && metric.getLabel() != null) {
-                    ResourcesResponse responseData = new ResourcesResponse();
-                    String prjCat = prjObjList.stream()
-                                   .filter(prj -> prj.get("prjId").equals(metric.getLabel().getPrjId()))
-                                   .map(prj -> prj.get("prjCat"))
-                                   .findFirst().orElse(null);
-                    responseData.setPrjCat(prjCat);
-                    responseData.setRunStatus(metric.getEvent() != null ? "RUNNING":"");
-                    responseData.setRscId(metric.getLabel().getRscId());
-                    responseData.setSvcType(metric.getLabel().getSvcType());
-                    responseData.setAccount("");
-                    responseData.setSystem("");
-                    responseData.setEnv(metric.getLabel().getEnv());
-                    responseData.setLocId(metric.getLabel().getLocId());
-                    responseData.setTarget(metric.getLabel().getTarget());
-                    responseData.setProject(metric.getLabel().getPrjId());
-                    responseData.setGpu(new ResourcesWithRatioQuota(
-                            metric.getDb() !=null ? metric.getDb().getGpuLimitDevices() : 0,
-                            metric.getGpuRequestDevices(),
-                            (metric.getDb() !=null ? metric.getDb().getGpuLimitDevices() : 0) == 0 ?
-                                    0 : Double.parseDouble(String.format("%.2f", metric.getGpuRequestDevices() * 100 / metric.getDb().getGpuLimitDevices())),
-                            metric.getGpuAllocatedUsagePercent(),
-                            metric.getGpuCount().getUsage5(),
-                            metric.getGpuCount().getUsage10(),
-                            metric.getGpuCount().getUsage15(),
-                            metric.getGpuCount().getUsage20(),
-                            metric.getGpuCount().getUsage25(),
-                            metric.getGpuCount().getUsage30(),
-                            metric.getGpuCount().getUsage35(),
-                            metric.getGpuCount().getUsage40(),
-                            metric.getGpuCount().getUsage45(),
-                            metric.getGpuCount().getUsage50(),
-                            metric.getGpuCount().getUsage55(),
-                            metric.getGpuCount().getUsage60(),
-                            metric.getGpuCount().getUsage65(),
-                            metric.getGpuCount().getUsage70(),
-                            metric.getGpuCount().getUsage75(),
-                            metric.getGpuCount().getUsage80(),
-                            metric.getGpuCount().getUsage85(),
-                            metric.getGpuCount().getUsage90(),
-                            metric.getGpuCount().getUsage95(),
-                            metric.getGpuCount().getUsage100()
-                    ));
-
-                    responseData.setCpu(new ResourcesWithRatioQuota(
-                            metric.getDb() != null ? metric.getDb().getCpuLimitCores() : 0,
-                            metric.getCpuAllocatedUsageCores(),
-                            (metric.getDb() != null ? metric.getDb().getCpuLimitCores() : 0) == 0 ?
-                                    0 : Double.parseDouble(String.format("%.2f", metric.getCpuRequestCores() * 100 / metric.getDb().getCpuLimitCores())),
-                            metric.getCpuAllocatedUsagePercent(),
-                            metric.getCpuCount().getUsage5(),
-                            metric.getCpuCount().getUsage10(),
-                            metric.getCpuCount().getUsage15(),
-                            metric.getCpuCount().getUsage20(),
-                            metric.getCpuCount().getUsage25(),
-                            metric.getCpuCount().getUsage30(),
-                            metric.getCpuCount().getUsage35(),
-                            metric.getCpuCount().getUsage40(),
-                            metric.getCpuCount().getUsage45(),
-                            metric.getCpuCount().getUsage50(),
-                            metric.getCpuCount().getUsage55(),
-                            metric.getCpuCount().getUsage60(),
-                            metric.getCpuCount().getUsage65(),
-                            metric.getCpuCount().getUsage70(),
-                            metric.getCpuCount().getUsage75(),
-                            metric.getCpuCount().getUsage80(),
-                            metric.getCpuCount().getUsage85(),
-                            metric.getCpuCount().getUsage90(),
-                            metric.getCpuCount().getUsage95(),
-                            metric.getCpuCount().getUsage100()
-                    ));
-
-                    responseData.setMemory(new ResourcesWithRatioQuota(
-                            metric.getDb() != null ? metric.getDb().getMemoryLimitBytes() : 0 ,
-                            metric.getMemoryAllocatedUsageBytes(),
-                            (metric.getDb() != null ? metric.getDb().getMemoryLimitBytes() : 0) == 0 ?
-                                    0 : Double.parseDouble(String.format("%.2f", metric.getMemoryRequestBytes() * 100 / metric.getDb().getMemoryLimitBytes())),
-                            metric.getMemoryAllocatedUsagePercent(),
-                            metric.getMemoryCount().getUsage5(),
-                            metric.getMemoryCount().getUsage10(),
-                            metric.getMemoryCount().getUsage15(),
-                            metric.getMemoryCount().getUsage20(),
-                            metric.getMemoryCount().getUsage25(),
-                            metric.getMemoryCount().getUsage30(),
-                            metric.getMemoryCount().getUsage35(),
-                            metric.getMemoryCount().getUsage40(),
-                            metric.getMemoryCount().getUsage45(),
-                            metric.getMemoryCount().getUsage50(),
-                            metric.getMemoryCount().getUsage55(),
-                            metric.getMemoryCount().getUsage60(),
-                            metric.getMemoryCount().getUsage65(),
-                            metric.getMemoryCount().getUsage70(),
-                            metric.getMemoryCount().getUsage75(),
-                            metric.getMemoryCount().getUsage80(),
-                            metric.getMemoryCount().getUsage85(),
-                            metric.getMemoryCount().getUsage90(),
-                            metric.getMemoryCount().getUsage95(),
-                            metric.getMemoryCount().getUsage100()
-                    ));
-                    result.add(responseData);
-                }
-            }
-        }
-        return result;
-    }
