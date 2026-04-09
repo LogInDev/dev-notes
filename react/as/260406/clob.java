0d9fa26fe852a189da6b3e2c529ef0242b1d1ac4
SaeRom Choi <fhazlt303@gmail.com>
2026-04-09 09:01:19 +0900

    Update clob.java

--- react/as/260406/clob.java
+++ react/as/260406/clob.java
@@ -1,39 +1,49 @@
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class ApiActionHistoryDetail implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    public List<HcpApiSvcIp> saveAllowIps(Long svcId, DrmAllowIpSaveRequest requestIps, Account account) throws Exception {
+        insertAllowIpList(svcId, requestIps.getCreatedList(), account);
 
-    private List<HistoryDetailDTO> keyList;
-    private AllowedIpDetailDTO allowedIp;
-    private RootKeyDetailDTO rootKey;
-}
+        updateAllowIpList(svcId, requestIps.getUpdatedList(), account);
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class AllowedIpDetailDTO implements Serializable {
+        deleteAllowIpList(svcId, requestIps.getDeletedList());
 
-    private static final long serialVersionUID = 1L;
+        Set<String> added = Set.of(
+                requestIps.getCreatedList().stream()
+                        .map(DrmAllowIpRequest::getIp).collect(Collectors.toSet()).toString(),
+                requestIps.getUpdatedList().stream()
+                        .map(DrmAllowIpRequest::getIp).collect(Collectors.toSet()).toString()
+                );
+        List<Long> updatedIpIds = requestIps.getUpdatedList().stream()
+                .map(DrmAllowIpRequest::getIpId).collect(Collectors.toList());
+        Set<String> updatedIps = new HashSet<>(hcpApiSvcIpMapper.getAllowedIpListById(updatedIpIds));
+        Set<String> removed = Set.of(
+                requestIps.getDeletedList().stream()
+                .map(DrmAllowIpRequest::getIp).collect(Collectors.toSet()).toString(), updatedIps.toString());
 
-    private List<String> added;
-    private List<String> updated;
-    private List<String> removed;
-}
+        AllowedIpDetailDTO allowedIp = AllowedIpDetailDTO.builder()
+                .added(added)
+                .removed(removed)
+                .build();
+        ApiActionHistoryDetail detailInfo = ApiActionHistoryDetail.builder()
+                .keyList(null)
+                .allowedIp(allowedIp)
+                .rootKey(null)
+                .build();
+
+        commonService.actionHistory(svcId, ActHistType.ADM, account.getAccountId(), detailInfo);
+
+        return hcpApiSvcIpMapper.getAllowedIpList(svcId);
+    }
 
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class RootKeyDetailDTO implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
-    private String previous;
-    private String current;
-}
+    <select id="getAllowedIpListById" resultType="String">
+        SELECT IP
+        FROM HCP_API_SVC_IP
+        WHERE 0=1
+        <if test="ipIds != null and ipIds.size() > 0">
+            OR IP_ID IN
+            <foreach item="ipId" collection="ipIds" open="(" separator="," close=")">
+                #{ipId}
+            </foreach>
+        </if>
+    </select>
